880b7f81a4c4eaa4f19ccdfa56112ca4ae9f6324
grhmdev <grhmdev@users.noreply.github.com>
2023-09-05 10:49:51 +0100

    Refactor process table components

--- frontend/src/components/Process/ListTable.js
+++ frontend/src/components/Process/ListTable.js
@@ -1,6 +1,54 @@
 import classes from "./ProcessList.module.css";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
+// Component to display processes in a table with sortable columns,
+// clicking a header cell should enable sorting processes for that column,
+// clicking a header cell already selected for sorting should toggle sort
+// direction
+const ListTable = ({ processes }) => {
+   const [sortProperty, setSortProperty] = useState("cpu_usage_percent");
+   const [sortAscending, setSortAscending] = useState(false);
+
+   const handleSort = useCallback(
+      (columnSortName) => {
+         if (sortProperty === columnSortName) {
+            // Toggle sort direction
+            setSortAscending((sortDirection) => !sortDirection);
+         } else {
+            // Change column to sort by
+            setSortProperty(columnSortName);
+         }
+      },
+      [sortProperty, setSortAscending, setSortProperty]
+   );
+
+   const sortedProcs = sortProcsByProperty(
+      processes,
+      sortProperty,
+      sortAscending
+   );
+   const tableRows = sortedProcs.map((proc) => (
+      <TableProcessRow key={proc.pid} proc={proc} />
+   ));
+
+   return (
+      <table className={classes["proc-table"]}>
+         <thead>
+            <TableHeaderRow
+               sortProperty={sortProperty}
+               sortAscending={sortAscending}
+               handleSort={handleSort}
+            />
+         </thead>
+         <tbody>{tableRows.length > 0 ? tableRows : <EmptyRow />}</tbody>
+      </table>
+   );
+};
+
+export default ListTable;
+
+// Empty/placeholder <tr> element to be displayed
+// when the table has no data to display
 const EmptyRow = () => (
    <>
       <tr>
@@ -13,113 +61,93 @@ const EmptyRow = () => (
    </>
 );
 
+const TableHeaderRow = ({ sortProperty, sortAscending, handleSort }) => (
+   <tr>
+      <TableHeader
+         label="PID"
+         sortName="pid"
+         selectedForSorting={sortProperty === "pid"}
+         sortDirection={sortAscending}
+         handleSort={handleSort}
+      />
+      <TableHeader
+         label="PPID"
+         sortName="ppid"
+         selectedForSorting={sortProperty === "ppid"}
+         sortDirection={sortAscending}
+         handleSort={handleSort}
+      />
+      <TableHeader
+         label="Name"
+         sortName="name"
+         selectedForSorting={sortProperty === "name"}
+         sortDirection={sortAscending}
+         handleSort={handleSort}
+      />
+      <TableHeader
+         label="CPU"
+         sortName="cpu_usage_percent"
+         selectedForSorting={sortProperty === "cpu_usage_percent"}
+         sortDirection={sortAscending}
+         handleSort={handleSort}
+      />
+      <TableHeader
+         label="Mem"
+         sortName="mem_usage_percent"
+         selectedForSorting={sortProperty === "mem_usage_percent"}
+         sortDirection={sortAscending}
+         handleSort={handleSort}
+      />
+   </tr>
+);
+
+// Clickable <th> element
+// Arguments:
+// - label: Displayed header text
+// - sortName: The property name or ID passed back to the sort handler
+// - handleSort: Sort handler callback
+// - sortDirection:
 const TableHeader = ({
    label,
    sortName,
    selectedForSorting,
    handleSort,
    sortDirection,
-}) => {
-   return (
-      <th
-         className={classes.sortable}
-         onClick={() => {
-            handleSort(sortName);
-         }}
-      >
-         {label}
-         {selectedForSorting && sortDirection && <span>&#8595;</span>}
-         {selectedForSorting && !sortDirection && <span>&#8593;</span>}
-      </th>
-   );
-};
+}) => (
+   <th
+      className={classes.sortable}
+      onClick={() => {
+         handleSort(sortName);
+      }}
+   >
+      {label}
+      {selectedForSorting && sortDirection && <span>&#8595;</span>}
+      {selectedForSorting && !sortDirection && <span>&#8593;</span>}
+   </th>
+);
 
-const ListTable = ({ processes }) => {
-   const [sortColumn, setSortColumn] = useState("cpu_usage_percent");
-   const [sortAscending, setSortAscending] = useState(false);
+// <tr> element to display process information
+const TableProcessRow = ({ proc }) => (
+   <tr title={proc.command}>
+      <td>{proc.pid}</td>
+      <td>{proc.ppid}</td>
+      <td>{proc.name}</td>
+      <td>{proc.cpu_usage_percent.toFixed(1)}%</td>
+      <td>{proc.mem_usage_percent.toFixed(1)}%</td>
+   </tr>
+);
 
-   const handleSort = (columnSortName) => {
-      if (sortColumn === columnSortName) {
-         // Toggle sort direction
-         setSortAscending((sortDirection) => !sortDirection);
-         return;
-      } else {
-         // Change column to sort by
-         setSortColumn(columnSortName);
-      }
-   };
-
-   let sortedProcs = [...processes];
+// Returns a list of procs sorted by an object property
+const sortProcsByProperty = (procs, sortProperty, sortAscending) => {
+   let sortedProcs = [...procs];
    sortedProcs.sort((a, b) => {
-      if (a[sortColumn] > b[sortColumn]) {
+      if (a[sortProperty] > b[sortProperty]) {
          return sortAscending ? 1 : -1;
       }
-      if (a[sortColumn] < b[sortColumn]) {
+      if (a[sortProperty] < b[sortProperty]) {
          return sortAscending ? -1 : 1;
       }
       return 0;
    });
-   const tableRows =
-      sortedProcs.length > 0 ? (
-         sortedProcs.map((proc) => (
-            <tr key={proc.pid} title={proc.command}>
-               <td>{proc.pid}</td>
-               <td>{proc.ppid}</td>
-               <td>{proc.name}</td>
-               <td>{proc.cpu_usage_percent.toFixed(1)}%</td>
-               <td>{proc.mem_usage_percent.toFixed(1)}%</td>
-            </tr>
-         ))
-      ) : (
-         <EmptyRow />
-      );
-
-   const tableHeaderRow = (
-      <tr>
-         <TableHeader
-            label="PID"
-            sortName="pid"
-            selectedForSorting={sortColumn === "pid"}
-            sortDirection={sortAscending}
-            handleSort={handleSort}
-         />
-         <TableHeader
-            label="PPID"
-            sortName="ppid"
-            selectedForSorting={sortColumn === "ppid"}
-            sortDirection={sortAscending}
-            handleSort={handleSort}
-         />
-         <TableHeader
-            label="Name"
-            sortName="name"
-            selectedForSorting={sortColumn === "name"}
-            sortDirection={sortAscending}
-            handleSort={handleSort}
-         />
-         <TableHeader
-            label="CPU"
-            sortName="cpu_usage_percent"
-            selectedForSorting={sortColumn === "cpu_usage_percent"}
-            sortDirection={sortAscending}
-            handleSort={handleSort}
-         />
-         <TableHeader
-            label="Mem"
-            sortName="mem_usage_percent"
-            selectedForSorting={sortColumn === "mem_usage_percent"}
-            sortDirection={sortAscending}
-            handleSort={handleSort}
-         />
-      </tr>
-   );
-
-   return (
-      <table className={classes["proc-table"]}>
-         <thead>{tableHeaderRow}</thead>
-         <tbody>{tableRows}</tbody>
-      </table>
-   );
+   return sortedProcs;
 };
-
-export default ListTable;
--- frontend/src/components/Process/TreeTable.js
+++ frontend/src/components/Process/TreeTable.js
@@ -1,60 +1,93 @@
 import classes from "./ProcessList.module.css";
 
-const EmptyRow = () => (
-   <>
-      <tr>
-         <td />
-         <td />
-         <td />
-         <td />
-      </tr>
-   </>
-);
-
+// This component displays processes in a table in their hierarchical order -
+// rows for child processes are grouped under the row of their parent process,
+// determined by their PIDs and PPIDs.
 const TreeTable = ({ processes }) => {
-   const findChildProcs = (parent, nestPrefix) => {
-      const children = processes.filter((child) => child.ppid === parent.pid);
-      let allDescendants = [];
-      children.forEach((child) => {
-         child.pidPrefix = nestPrefix + " ";
-         allDescendants.push(child);
-         allDescendants.push(...findChildProcs(child, child.pidPrefix));
-      });
-      return allDescendants;
-   };
-
-   const rootProcs = processes.filter((proc) => proc.ppid === 0);
-   let tableRows = <EmptyRow/>
-   if (rootProcs.length > 0) {
-      const allProcs = [rootProcs[0], ...findChildProcs(rootProcs[0], " ")];
+   let tableRows = <EmptyRow />;
+   if (processes.length > 0) {
+      const rootProc = processes.filter((proc) => proc.ppid === 0)[0];
+      const allProcs = [
+         rootProc,
+         ...sortProcsDepthFirst(rootProc, processes, " "),
+      ];
       tableRows = allProcs.map((proc) => (
-         <tr key={proc.pid} title={proc.command}>
-            <td>
-               {proc.pidPrefix}
-               {proc.pid}
-            </td>
-            <td>{proc.name}</td>
-            <td>{proc.cpu_usage_percent.toFixed(1)}%</td>
-            <td>{proc.mem_usage_percent.toFixed(1)}%</td>
-         </tr>
+         <TableProcessRow key={proc.pid} proc={proc} />
       ));
    }
 
-   const tableHeaderRow = (
-      <tr>
-         <th>PID</th>
-         <th>Name</th>
-         <th>CPU</th>
-         <th>Mem</th>
-      </tr>
-   );
-
    return (
       <table className={classes["proc-table"]}>
-         <thead>{tableHeaderRow}</thead>
+         <thead>
+            <TableHeaderRow />
+         </thead>
          <tbody>{tableRows}</tbody>
       </table>
    );
 };
-
 export default TreeTable;
+
+// <tr> element displaying process information
+const TableProcessRow = ({ proc }) => (
+   <tr title={proc.command}>
+      <td>
+         {proc.pidPrefix}
+         {proc.pid}
+      </td>
+      <td>{proc.name}</td>
+      <td>{proc.cpu_usage_percent.toFixed(1)}%</td>
+      <td>{proc.mem_usage_percent.toFixed(1)}%</td>
+   </tr>
+);
+
+// Empty/placeholder <tr> element to be displayed
+// when the table has no data to display
+const EmptyRow = () => (
+   <tr>
+      <td />
+      <td />
+      <td />
+      <td />
+   </tr>
+);
+
+const TableHeaderRow = () => (
+   <tr>
+      <th>PID</th>
+      <th>Name</th>
+      <th>CPU</th>
+      <th>Mem</th>
+   </tr>
+);
+
+/*
+Returns a sorted list of all descendant processes of a parent process
+as a flattened tree structure.
+Example:
+   Inputs:
+      parent = {PID: 1}
+      allProcs = [
+                   {PID: 1, PPID: 0},
+                   {PID: 2, PPID: 1},
+                   {PID: 3, PPID: 1},
+                   {PID: 4, PPID: 2} <-- Child of PID 2
+                 ]
+   Output:
+      [
+         {PID: 2, ...},
+         {PID: 4, ...}, <-- Child of PID 2 moved
+         {PID: 3, ...}
+      ]
+*/
+const sortProcsDepthFirst = (parent, allProcs, nestPrefix) => {
+   const children = allProcs.filter((child) => child.ppid === parent.pid);
+   let allDescendants = [];
+   children.forEach((child) => {
+      child.pidPrefix = nestPrefix + " ";
+      allDescendants.push(child);
+      allDescendants.push(
+         ...sortProcsDepthFirst(child, allProcs, child.pidPrefix)
+      );
+   });
+   return allDescendants;
+};
